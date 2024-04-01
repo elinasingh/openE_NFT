bff23e16a26979557394c18b25dd3b8be17a02ec
Elina Singh <elinasingh1997@gmail.com>
2024-04-01 21:03:50 +0530

    Changing files to my preference

--- src/openE_frontend/src/index.js
+++ src/openE_frontend/src/index.js
@@ -1,19 +1,6 @@
-import { openE_backend } from "../../declarations/openE_backend";
-
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await openE_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	
+/******/ 	
+/******/ })()
+;
